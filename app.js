ab23d4c88043fc0bd2dbd47ef93da98ee0c396f7
Tom Gagliano <tgaglianoll@gmail.com>
2025-01-20 20:27:36 -0500

    add db.js file for mongoDB database

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const app = express();
+const connectDB = require("./db");
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
+
+connectDB();
 
 const PORT = process.env.PORT || 3003;
 app.listen(PORT, () => {
