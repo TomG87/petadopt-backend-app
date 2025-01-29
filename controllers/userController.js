37aa887ddaa26b5757a345aa418cf6f7c9bdeb39
Tom Gagliano <tgaglianoll@gmail.com>
2025-01-29 18:32:00 -0500

    add loginUser functionality to userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -44,4 +44,25 @@ const getAllUsers = async (req, res) => {
     res.status(500).json({ error: "Failed to fetch users" });
   }
 };
-module.exports = { addUser, getAllUsers };
+
+const loginUser = async ( req, res ) => {
+  try {
+    const { email, password } = req.body
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({"User not found"});
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({message: "Invalid credentials"});
+    }
+
+    res.status(200).json({ message: "Login Successful", user });
+  } catch (error) {
+    console.log("Error logging in: ", error.message);
+    res.status(500).json({ message: "Error logging in", error: error.message });
+  }
+};
+module.exports = { addUser, getAllUsers, loginUser };
