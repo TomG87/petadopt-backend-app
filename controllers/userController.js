4c762b0d9accbde40bb3affd47a86b8b4595dfe2
Tom Gagliano <tgaglianoll@gmail.com>
2025-01-30 15:46:10 -0500

    add deleteUser functionality to userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -64,5 +64,23 @@ const loginUser = async ( req, res ) => {
     console.log("Error logging in: ", error.message);
     res.status(500).json({ message: "Error logging in", error: error.message });
   }
-};
-module.exports = { addUser, getAllUsers, loginUser };
+
+  const deleteUser = async (req, res) => {
+    try {
+      const { userId } = req.params;
+  
+      const user = await User.findById(userId); 
+      if (!user) {
+        return res.status(404).json({ message: "User not found" });
+      }
+  
+      await User.findByIdAndDelete(userId);
+  
+      res.status(200).json({ message: "User deleted successfully" });
+    } catch (error) {
+      console.error("Error deleting user:", error);
+      res.status(500).json({ message: "Internal server error" });
+    }
+  };
+  
+  module.exports = { addUser, getAllUsers, loginUser, deleteUser };
