87b8f161826dce1651b5ccfd2b727e419a1ff144
Tom Gagliano <tgaglianoll@gmail.com>
2025-02-01 15:05:46 -0500

    add user routes to userController

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,24 @@
 const express = require("express");
 const router = express.Router();
 
-const { addUser, getAllUsers, loginUser, deleteUser, updateUser  } = require("./controllers/userController");
+const {
+  addUser,
+  getAllUsers,
+  loginUser,
+  deleteUser,
+  updateUser,
+} = require("./controllers/userController");
 
-const 
+const {
+  addPet,
+  getAllPets,
+  getPetById,
+  deletePet,
+  updatePet,
+} = require("./controllers/petController");
+
+router.post("/users", addUser);
+router.get("/users", getAllUsers);
+router.post("/users", loginUSer);
+router.delete("/users/:userId", deleteUser);
+router.put("/users/:userId", updateUser);
