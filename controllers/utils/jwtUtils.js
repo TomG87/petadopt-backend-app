24708214253c95754329172979c1f6cad3744aa4
Tom Gagliano <tgaglianoll@gmail.com>
2025-03-05 19:17:50 -0500

    add initial code for jwt token file

--- controllers/utils/jwtUtils.js
+++ controllers/utils/jwtUtils.js
@@ -1,1 +1,17 @@
 const jwt = require("jsonwebtoken");
+// const secretKey = "will add later"
+
+const generateToken = (userId) => {
+  return jwt.sign({ userId }, secretKey, { expiresIn: "2h" });
+};
+
+const verifyToken = (token) => {
+  try {
+    const decoded = jwt.verify(token, secretKey);
+    return decoded;
+  } catch (err) {
+    return null;
+  }
+};
+
+module.exports = { generateToken, verifyToken };
