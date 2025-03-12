0c185875b5861c4127289e281e2e84c731e34428
Tom Gagliano <tgaglianoll@gmail.com>
2025-03-12 18:41:03 -0400

    add authenticate middleware to protected routes on routes file

--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const authenticate = require("./middleware/authenticate");
 
 const {
   addUser,
@@ -25,20 +26,20 @@ const {
 } = require("./controllers/messageController");
 
 router.post("/users", addUser);
-router.get("/users", getAllUsers);
+router.get("/users", authenticate, getAllUsers);
 router.post("/users/login", loginUser);
-router.delete("/users/:userId", deleteUser);
-router.put("/users/:userId", updateUser);
-router.get("/users/:userId/pets", getPetsByUserId);
+router.delete("/users/:userId", authenticate, deleteUser);
+router.put("/users/:userId", authenticate, updateUser);
+router.get("/users/:userId/pets", authenticate, getPetsByUserId);
 
-router.post("/pets", addPet);
-router.get("/pets", getAllPets);
-router.get("/pets/:petId", getPetById);
-router.delete("/pets/:petId", deletePet);
-router.put("/pets/:petId", updatePet);
+router.post("/pets", authenticate, addPet);
+router.get("/pets", authenticate, getAllPets);
+router.get("/pets/:petId", authenticate, getPetById);
+router.delete("/pets/:petId", authenticate, deletePet);
+router.put("/pets/:petId", authenticate, updatePet);
 
-router.post("/messages/thread", createMessageThread);
-router.post("/messages/:threadId", sendMessage);
-router.delete("/messages/:threadId/:messageId", deleteMessage);
+router.post("/messages/thread", authenticate, createMessageThread);
+router.post("/messages/:threadId", authenticate, sendMessage);
+router.delete("/messages/:threadId/:messageId", authenticate, deleteMessage);
 
 module.exports = router;
